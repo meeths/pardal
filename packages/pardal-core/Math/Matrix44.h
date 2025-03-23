2e88577342f2921998c0ee6192435696fbb753e3
Sisco Macias <meeths@gmail.com>
2025-03-23 12:33:54 +0100

    Matrix44

---
+++ packages/pardal-core/Math/Matrix44.h
@@ -0,0 +1,13 @@
+
+#pragma once
+#include <glm/mat4x4.hpp>
+#include <glm/ext/matrix_transform.hpp>
+// Created on 2025-03-23 by sisco
+
+namespace pdl
+{
+    namespace Math
+    {
+        using Matrix44 = glm::mat4;
+    }
+}
