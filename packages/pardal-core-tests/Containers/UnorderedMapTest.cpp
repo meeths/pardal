ad4dbebf4b5e4ddeacaf27fddc9b9ccb0e49e8d6
Sisco Macias <siscomacias@gmail.com>
2026-01-11 22:56:24 +0100

    Update UnorderedMapTest for concatenation

--- packages/pardal-core-tests/Containers/UnorderedMapTest.cpp
+++ packages/pardal-core-tests/Containers/UnorderedMapTest.cpp
@@ -82,5 +82,39 @@ namespace pdl
             EXPECT_EQ(a.size(), 5u);
             EXPECT_EQ(b.size(), 10u);
         }
+
+        TEST(UnorderedMapTest, ConcatCopiesAndOverwrites)
+        {
+            UnorderedMap<std::string, int> a;
+            a["x"] = 1;
+            a["y"] = 2;
+
+            UnorderedMap<std::string, int> b;
+            b["y"] = 20; // will overwrite
+            b["z"] = 3;
+
+            a.concat(b);
+
+            EXPECT_EQ(a.size(), 3u);
+            EXPECT_EQ(a.at("x"), 1);
+            EXPECT_EQ(a.at("y"), 20);
+            EXPECT_EQ(a.at("z"), 3);
+        }
+
+        TEST(UnorderedMapTest, ConcatMoveMovesValues)
+        {
+            UnorderedMap<std::string, std::string> a;
+            a["a"] = "one";
+
+            UnorderedMap<std::string, std::string> b;
+            b["b"] = "two";
+            b["a"] = "ONE"; // overwrites
+
+            a.concat(std::move(b));
+
+            EXPECT_EQ(a.size(), 2u);
+            EXPECT_EQ(a.at("a"), std::string("ONE"));
+            EXPECT_EQ(a.at("b"), std::string("two"));
+        }
     }
 }
