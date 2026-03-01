b3dc720fcabfc36876b281e8c576a911f169c201
Sisco Macias <siscomacias@gmail.com>
2026-03-01 13:04:20 +0100

    [Core] Changed Log singleton to service locator pattern

--- packages/pardal-core/Log/Log.h
+++ packages/pardal-core/Log/Log.h
@@ -1,21 +1,21 @@
 #pragma once
-#include <Base/ISingleton.h>
+#include <Base/ServiceLocator.h>
 #include <Log/ILogger.h>
 #include <Memory/SharedPointer.h>
 #include <String/String.h>
 #include <Containers/Vector.h>
 #include <Threading/SRWLock.h>
 
-#define pdlLogError(x, ...) pdl::Log::Instance().LogError(x, __VA_ARGS__)
-#define pdlLogWarning(x, ...) pdl::Log::Instance().LogWarning(x, __VA_ARGS__)
-#define pdlLogInfo(x, ...) pdl::Log::Instance().LogInfo(x, __VA_ARGS__)
-#define pdlLogFlush() pdl::Log::Instance().Flush()
+#define pdlLogError(x, ...) pdl::ServiceLocator<pdl::Log>::Ref().LogError(x, __VA_ARGS__)
+#define pdlLogWarning(x, ...) pdl::ServiceLocator<pdl::Log>::Ref().LogWarning(x, __VA_ARGS__)
+#define pdlLogInfo(x, ...) pdl::ServiceLocator<pdl::Log>::Ref().LogInfo(x, __VA_ARGS__)
+#define pdlLogFlush() pdl::ServiceLocator<pdl::Log>::Ref().Flush()
 
 namespace pdl
 {
 class ILogger;
 	
-class Log : public ISingleton<Log>
+class Log
 {
 public:
 
